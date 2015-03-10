839ea7c5ab5560d0ba8ba438dec4943db10fd091
acstll <19@8302.net>
2015-03-10 17:19:15 +0100

    silly change to test/browser.js

--- test/browser.js
+++ test/browser.js
@@ -4,12 +4,23 @@ var el = document.querySelector('.slides');
 
 var alter = function (action, options) {
   var el = this.el;
-  if (this.state === 'current') {
-    el.style.fontWeight = 'bold';
-    el.style.color = 'pink';
-  } else {
-    el.style.fontWeight = 'normal';
-    el.style.color = 'black';
+
+  switch (this.state) {
+    case 'current':
+      el.style.fontWeight = 'bold';
+      el.style.color = 'red';
+      break;
+    case 'previous':
+      el.style.fontWeight = 'normal';
+      el.style.color = 'black';
+      break;
+    case 'next':
+      el.style.fontWeight = 'normal';
+      el.style.color = 'yellow';
+      break;
+    default:
+      el.style.fontWeight = 'normal';
+      el.style.color = '#ccc';
   }
 
   el.classList.add(this.state);
